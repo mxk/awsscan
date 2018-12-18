47e9dc21680ec77b7caebeda7fbe9ab29387d8a0
Maxim Khitrov <max@mxcrypt.com>
2018-12-17 21:27:44 -0500

    Add aws_cloudwatch_dashboard resource type

--- scan/svc/cloudwatch.go
+++ scan/svc/cloudwatch.go
@@ -2,6 +2,7 @@ package svc
 
 import (
 	"github.com/LuminalHQ/cloudcover/awsscan/scan"
+	"github.com/LuminalHQ/cloudcover/x/tfx"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatch"
 )
 
@@ -9,4 +10,15 @@ type cloudwatchSvc struct{ *scan.Ctx }
 
 var _ = scan.Register(cloudwatch.EndpointsID, cloudwatch.New, cloudwatchSvc{},
 	[]cloudwatch.DescribeAlarmsInput{},
+	[]cloudwatch.ListDashboardsInput{},
 )
+
+//
+// Post-processing
+//
+
+func (s cloudwatchSvc) Dashboards(out *cloudwatch.ListDashboardsOutput) error {
+	return s.ImportResources("aws_cloudwatch_dashboard", tfx.AttrGen{
+		"id": s.Strings(out.DashboardEntries, "DashboardName"),
+	})
+}
