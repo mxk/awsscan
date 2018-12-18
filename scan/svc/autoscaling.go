3b353c5d58136a18b74337046c2edca1550491ca
Maxim Khitrov <max@mxcrypt.com>
2018-12-17 21:22:51 -0500

    Add autoscaling resource types

--- scan/svc/autoscaling.go
+++ scan/svc/autoscaling.go
@@ -2,6 +2,7 @@ package svc
 
 import (
 	"github.com/LuminalHQ/cloudcover/awsscan/scan"
+	"github.com/LuminalHQ/cloudcover/x/tfx"
 	"github.com/aws/aws-sdk-go-v2/service/autoscaling"
 )
 
@@ -11,7 +12,14 @@ var _ = scan.Register(autoscaling.EndpointsID, autoscaling.New, autoscalingSvc{}
 	[]autoscaling.DescribeAutoScalingGroupsInput{},
 	[]autoscaling.DescribeAutoScalingInstancesInput{},
 	[]autoscaling.DescribeLaunchConfigurationsInput{},
+	[]autoscaling.DescribePoliciesInput{},
+	[]autoscaling.DescribeScheduledActionsInput{},
 )
+
+func (s autoscalingSvc) DescribeLifecycleHooks(dasg *autoscaling.DescribeAutoScalingGroupsOutput) (q []autoscaling.DescribeLifecycleHooksInput) {
+	s.Split(&q, "AutoScalingGroupName", dasg.AutoScalingGroups, "AutoScalingGroupName")
+	return
+}
 
 func (s autoscalingSvc) DescribeNotificationConfigurations(dasg *autoscaling.DescribeAutoScalingGroupsOutput) (q []autoscaling.DescribeNotificationConfigurationsInput) {
 	s.Group(&q, "AutoScalingGroupNames", dasg.AutoScalingGroups, "AutoScalingGroupName", 1600)
@@ -21,9 +29,47 @@ func (s autoscalingSvc) DescribeNotificationConfigurations(dasg *autoscaling.Des
 	return
 }
 
-func (s autoscalingSvc) DescribePolicies(dasg *autoscaling.DescribeAutoScalingGroupsOutput) (q []autoscaling.DescribePoliciesInput) {
-	s.Split(&q, "AutoScalingGroupName", dasg.AutoScalingGroups, "AutoScalingGroupName")
-	return
+//
+// Post-processing
+//
+
+func (s autoscalingSvc) AutoScalingGroups(out *autoscaling.DescribeAutoScalingGroupsOutput) error {
+	return s.ImportResources("aws_autoscaling_group", tfx.AttrGen{
+		"id": s.Strings(out.AutoScalingGroups, "AutoScalingGroupName"),
+	})
+}
+
+func (s autoscalingSvc) LaunchConfigurations(out *autoscaling.DescribeLaunchConfigurationsOutput) error {
+	return s.ImportResources("aws_launch_configuration", tfx.AttrGen{
+		"id": s.Strings(out.LaunchConfigurations, "LaunchConfigurationName"),
+	})
+}
+
+func (s autoscalingSvc) LifecycleHooks(out *autoscaling.DescribeLifecycleHooksOutput) error {
+	name := s.Strings(out.LifecycleHooks, "LifecycleHookName")
+	return s.MakeResources("aws_autoscaling_lifecycle_hook", tfx.AttrGen{
+		"id":                     name,
+		"autoscaling_group_name": s.Strings(out.LifecycleHooks, "AutoScalingGroupName"),
+		"name":                   name,
+	})
+}
+
+func (s autoscalingSvc) Policies(out *autoscaling.DescribePoliciesOutput) error {
+	name := s.Strings(out.ScalingPolicies, "PolicyName")
+	return s.MakeResources("aws_autoscaling_policy", tfx.AttrGen{
+		"id":                     name,
+		"autoscaling_group_name": s.Strings(out.ScalingPolicies, "AutoScalingGroupName"),
+		"name":                   name,
+	})
+}
+
+func (s autoscalingSvc) ScheduledActions(out *autoscaling.DescribeScheduledActionsOutput) error {
+	name := s.Strings(out.ScheduledUpdateGroupActions, "ScheduledActionName")
+	return s.MakeResources("aws_autoscaling_schedule", tfx.AttrGen{
+		"id":                     name,
+		"autoscaling_group_name": s.Strings(out.ScheduledUpdateGroupActions, "AutoScalingGroupName"),
+		"scheduled_action_name":  name,
+	})
 }
 
 func (s autoscalingSvc) mockOutput(out interface{}) {
